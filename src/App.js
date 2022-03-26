9a5e87456805f500b4fa030a1a816a1c04894a3b
Arseniy Boltzmunn <ars.boltz@gmail.com>
2022-03-26 19:38:41 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './Styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./UI/selector/MySelect";
+import MyInput from "./UI/imput/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -14,12 +15,7 @@ function App() {
             {id: 3, title: 'JavaScript', body: 'Description'}
         ]
     )
-    // const [posts2, setPosts2] = useState([
-    //         {id: 1, title: 'Python', body: 'Description'},
-    //         {id: 2, title: 'Python 2 ', body: 'Description'},
-    //         {id: 3, title: 'Python 3', body: 'Description'}
-    //     ]
-    // )
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
@@ -29,27 +25,47 @@ function App() {
 
     }
     const [selectedSort, setSelectedSort] = useState('')
+
+
+    const [searchQuery, setSearchQuery] = useState('')
+
     const sortPosts = (sort) => {
         setSelectedSort(sort)
-        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-
     }
+
+
+    const sortedPosts = useMemo(() => {
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts
+    }, [selectedSort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+    }, [searchQuery, sortedPosts])
+
     return (
         <div>
             <Counter/>
             <ClassCounter/>
+            <MyInput
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                placeholder='search...'
+            />
             <PostForm create={createPost}/>
-                <MySelect
-                    value={selectedSort}
-                    onChange={sortPosts}
-                    defaultValue='sort'
-                    options={[
-                        {value: 'title', name: 'name'},
-                        {value: 'body', name: 'description'}
-                    ]}
-                />
-            {posts.length
-                ? <PostList remove={removePost} posts={posts} title='post list JS'/>
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue='sort'
+                options={[
+                    {value: 'title', name: 'name'},
+                    {value: 'body', name: 'description'}
+                ]}
+            />
+            {sortedAndSearchedPosts.length
+                ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title='post list JS'/>
                 : <h1 style={{textAlign: 'center'}}>posts not found</h1>
             }
             {/*<PostList posts={posts2} title='post list Python'/>*/}
