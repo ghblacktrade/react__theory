7f812b6ffec7e8f2eee6688df0c10964b9a5a5df
Arseniy Boltzmunn <ars.boltz@gmail.com>
2022-03-26 15:56:55 +0300

    remove post

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './Styles/App.css'
 import PostList from "./components/PostList";
-import MyButton from "./UI/button/MyButton";
-import MyInput from "./UI/imput/MyInput";
 import PostForm from "./components/PostForm";
 
 function App() {
@@ -15,36 +13,31 @@ function App() {
             {id: 3, title: 'JavaScript', body: 'Description'}
         ]
     )
-    const [posts2, setPosts2] = useState([
-            {id: 1, title: 'Python', body: 'Description'},
-            {id: 2, title: 'Python 2 ', body: 'Description'},
-            {id: 3, title: 'Python 3', body: 'Description'}
-        ]
-    )
-    const addNewPost = (e) => {
-        e.preventDefault()
-        const newPost = {
-            id: Date.now(),
-            title,
-            body,
-        }
-
+    // const [posts2, setPosts2] = useState([
+    //         {id: 1, title: 'Python', body: 'Description'},
+    //         {id: 2, title: 'Python 2 ', body: 'Description'},
+    //         {id: 3, title: 'Python 3', body: 'Description'}
+    //     ]
+    // )
+    const createPost = (newPost) => {
         setPosts([...posts, newPost])
-        setTitle('')
-        setBody('')
     }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
 
-    const [body, setBody] = useState('')
-
+    }
 
     return (
         <div>
             <Counter/>
             <ClassCounter/>
-<PostForm/>
-            <PostList posts={posts} title='post list JS'/>
-            <PostList posts={posts2} title='post list Python'/>
+            <PostForm create={createPost}/>
+            {posts.length !== 0
+                ? <PostList remove={removePost} posts={posts} title='post list JS'/>
+                : <h1 style={{textAlign: 'center'}}>posts not found</h1>
+            }
+            {/*<PostList posts={posts2} title='post list Python'/>*/}
 
         </div>
     );
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,23 +3,37 @@ import MyInput from "../UI/imput/MyInput";
 import MyButton from "../UI/button/MyButton";
 
 
-const PostForm = (props) => {
+const PostForm = ({create}) => {
+    const [post, setPost] = useState({title: '', body: ''})
 
-    const [title, setTitle] = useState('')
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const newPost = {
+            ...post, id: Date.now()
+        }
+        create(newPost)
+        setPost({title: '', body: ''})
+    }
+
+
 
     return (
         <form>
             <MyInput
-                value={title}
-                onChange={e => setTitle(e.target.value)}
+                value={post.title}
+                onChange={e => setPost({...post, title: e.target.value})}
                 type='text'
-                placeholder='name post' />
+                placeholder='name post'
+
+            />
             <MyInput
-                value={body}
-                onChange={e => setBody(e.target.value)}
+                value={post.body}
+                onChange={e => setPost({...post, body: e.target.value})}
                 type='text'
-                placeholder='name post' />
-            <MyButton onClick={addNewPost}>Create post</MyButton>
+                placeholder='description post'
+            />
+            <MyButton onClick={addNewPost}>Create Post</MyButton>
         </form>
     )
 }
